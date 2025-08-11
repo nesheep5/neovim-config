77bb58e8aec4b07a89c7d79a1bf50c08c955ce09
shogo <shogo@shogonoMacBook-Pro.local>
2025-08-11 17:29:07 +0900

    copilot-chatの設定を修正

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -17,7 +17,7 @@ return {
   },
 
   opts = {
-    model = "claude-3.7-sonnet",
+    model = "claude-sonnet-4",
     debug = false,
     show_folds = false,
     context = "buffer", -- buffer, tabpage, visible
@@ -33,17 +33,17 @@ return {
       },
     },
     window = {
-      layout = 'vertical',  -- 'vertical', 'horizontal', 'float', 'replace', or a function that returns the layout
-      width = 0.4,          -- fractional width of parent, or absolute width in columns when > 1
-      height = 0.5,         -- fractional height of parent, or absolute height in rows when > 1
+      layout = 'vertical',    -- 'vertical', 'horizontal', 'float', 'replace', or a function that returns the layout
+      width = 0.4,            -- fractional width of parent, or absolute width in columns when > 1
+      height = 0.5,           -- fractional height of parent, or absolute height in rows when > 1
       -- Options below only apply to floating windows
-      relative = 'editor',  -- 'editor', 'win', 'cursor', 'mouse'
-      border = 'rounded',    -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
-      row = nil,            -- row position of the window, default is centered
-      col = nil,            -- column position of the window, default is centered
+      relative = 'editor',    -- 'editor', 'win', 'cursor', 'mouse'
+      border = 'rounded',     -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
+      row = nil,              -- row position of the window, default is centered
+      col = nil,              -- column position of the window, default is centered
       title = 'Copilot Chat', -- title of chat window
-      footer = nil,         -- footer of chat window
-      zindex = 1,           -- determines if window is on top or below other floating windows
+      footer = nil,           -- footer of chat window
+      zindex = 1,             -- determines if window is on top or below other floating windows
     }
   },
   -- See Commands section for default commands if you want to lazy load on them
