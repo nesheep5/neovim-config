1f31ebddecad6ab2756fad8bb75cfbcd628a46b5
shogo <shogo@shogonoMacBook-Pro.local>
2025-08-11 17:55:15 +0900

    非推奨を修正

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -30,7 +30,7 @@ return {
           'fileformat',
           'filetype',
           { function()
-            local buf_clients = vim.lsp.get_active_clients({ bufnr = 0 })
+            local buf_clients = vim.lsp.get_clients({ bufnr = 0 })
             if #buf_clients == 0 then
               return 'No LSP'
             end
